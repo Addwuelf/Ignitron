253a355150ba8e7e50d47ef7a2820a7b32b2fdaa
Addwuelf <addwuelf@yahoo.com>
2026-01-01 18:04:27 -0500

    Project has been fully set up.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,10 @@
 module org.example.ignitron {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
 
     requires org.controlsfx.controls;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
-    requires eu.hansolo.tilesfx;
 
     opens org.example.ignitron to javafx.fxml;
     exports org.example.ignitron;
--- src/main/java/org/example/ignitron/IgnitronController.java
+++ src/main/java/org/example/ignitron/IgnitronController.java
@@ -3,7 +3,7 @@ package org.example.ignitron;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class IgnitronController {
     @FXML
     private Label welcomeText;
 
